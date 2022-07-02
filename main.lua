ed7e99fc6ea975088f673abae938e547132e7af3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-01 23:44:05 -0400

    set up map, load libraries

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
+dofile("libraries/map_prepack.lua")
+dofile("libraries/ecs.lua")
+dofile("libraries/ecs.lua")
+
+local
+table_insert, map_size,ecs
+=
+table.insert, map_size,ecs
+
+
+local scale = 1
+
+local map = reset_map()
+
 function love.load()
 
 end
@@ -7,5 +21,5 @@ function love.update()
 end
 
 function love.draw()
-
+    for i = 1,map_size
 end
