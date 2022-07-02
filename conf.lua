85d7656f4c6e11139ba3086666427977832e3fa9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-01 23:32:11 -0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,3 +1,11 @@
 function love.conf(settings)
+
    settings.window.title = "Chompur"
+
+   settings.version = "11.4"
+
+   settings.window.width  = 1280
+   settings.window.height = 720
+   settings.window.resizable = false
+
 end
