bbda4b082a225bb357ad8ca5856895109a05dfca
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-07-02 00:27:09 -0400

    apply accurate spacing on re-sizable window

--- conf.lua
+++ conf.lua
@@ -5,7 +5,7 @@ function love.conf(settings)
    settings.version = "11.4"
 
    settings.window.width  = 1280
-   settings.window.height = 720
-   settings.window.resizable = false
+   settings.window.height = 700
+   settings.window.resizable = true
 
 end
--- main.lua
+++ main.lua
@@ -3,14 +3,53 @@ dofile("libraries/ecs.lua")
 dofile("libraries/ecs.lua")
 
 local
-table_insert, map_size,ecs
+table_insert, map_size,ecs,iterator_1d
 =
-table.insert, map_size,ecs
+table.insert, map_size,ecs,map_size*map_size
 
+-- default of 700
+local scale = 66
+local spacing = 4
 
-local scale = 1
+local tile_scale = 66
+local tile_spacing = 4
+
+local round_corners = 3
 
 local map = reset_map()
+
+function love.resize( width, height )
+    print("window was resized: " .. width .. ", " .. height)
+
+    local new_scale
+
+    if width <= height then
+        new_scale = width
+    else
+        new_scale = height
+    end
+
+    tile_scale = (new_scale / 700) * scale
+
+    tile_spacing = (new_scale / 700) * spacing
+
+
+
+end
+
+local function convert_1d_to_2d(i)
+    i = i - 1
+    local x = (i % 10) % 10
+    local y = math.floor(i / 10)
+
+    return({ x, y })
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
 
 function love.load()
 
@@ -21,5 +60,8 @@ function love.update()
 end
 
 function love.draw()
-    for i = 1,map_size
+    for i = 1,iterator_1d do
+        local x,y = unpack(convert_1d_to_2d(i))
+        love.graphics.rectangle( "line", x * (tile_scale + tile_spacing), y * (tile_scale + tile_spacing), tile_scale, tile_scale, round_corners, round_corners)
+    end
 end
